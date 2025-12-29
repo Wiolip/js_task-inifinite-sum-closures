bfb90e0a1dddeeb6f823ae2c12c6dbf679583801
Wioleta Lip <wiolip@hotmail.com>
2025-12-29 18:01:13 +0100

    initial

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -4,7 +4,26 @@
  * @return {function}
  */
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  return function adder(...args) {
+    // If called with no arguments: return sum and reset
+    if (args.length === 0) {
+      const result = sum;
+
+      sum = 0;
+
+      return result;
+    }
+
+    // Sum all arguments passed in this specific call
+    const currentBatch = args.reduce((acc, val) => acc + val, 0);
+
+    sum += currentBatch;
+
+    // Return the function itself to allow further chaining
+    return adder;
+  };
 }
 
 module.exports = makeInfinityAdder;
